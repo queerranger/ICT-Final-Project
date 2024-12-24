a2aba9abc5dcf1fea894d5f76843de6fbed1db0f
Ranger <mnipk1243@gmail.com>
2024-12-24 05:12:24 +0500

    changes made

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let button = document.getElementById("resbtn");
+let changebg = document.getElementById("herosection");
+
+button.addEventListener("click", function changebackground(){
+
+    
+    changebg.style.backgroundImage="url('logobg.jpg')";
+    changebg.style.backgroundSize="cover";
+    changebg.style.backgroundPosition="center";
+
+})
+
+
+
 
 
 
